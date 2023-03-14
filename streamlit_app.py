d9719bf648b811caf0a022cba1585523cc9d76e5
JoleneSFTraining <127357900+JoleneSFTraining@users.noreply.github.com>
2023-03-14 17:17:23 -0500

    Try this

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,10 +57,10 @@ if streamlit.button('Get Fruit Load List'):
 
 # Allow end user to add a fruit to the list
 streamlit.text("What fruit would you like to add?")
-new_fruit = streamlit.text_input('')
+ = streamlit.text_input('')
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + new_fruit +"')")
+    my_cur.execute("insert into fruit_load_list values ('"new_fruit"')")
     return "Thanks for adding " + new_fruit
 
 #add_my_fruit = streamlit.text_input('')
