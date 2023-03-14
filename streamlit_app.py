b7b77b919f70a7a488c256497aa7e788b196161d
JoleneSFTraining <127357900+JoleneSFTraining@users.noreply.github.com>
2023-03-14 10:03:53 -0500

    Update streamlit_app.py
    Added header for Fruit Advice!

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
 
 #New Section to display fruityvice api response
 import requests
