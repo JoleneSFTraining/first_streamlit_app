26c9a4a6f1c62bd1b9e192f79d9e19f1b80d41e0
JoleneSFTraining <127357900+JoleneSFTraining@users.noreply.github.com>
2023-03-14 17:02:30 -0500

    Added a catch line

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,7 @@ if streamlit.button('Get Fruit Load List'):
 
 # Allow end user to add a fruit to the list
 streamlit.text("What fruit would you like to add?")
+new_fruit = streamlit.text_input('')
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert info fruit_load_list values ('" + new_fruit +"')")
