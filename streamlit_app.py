8dbdc66b454f5430f0b695a792b1bd060ba10bd8
JoleneSFTraining <127357900+JoleneSFTraining@users.noreply.github.com>
2023-03-14 15:46:10 -0500

    ask for fruit added to list

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 # Allow end user to add a fruit to the list
 streamlit.text("What fruit would you like to add?")
-fruit_choice_to_add = streamlit.text_input('')
-streamlit.text("Thanks for adding " + fruit_choice_to_add)
+add_my_fruit = streamlit.text_input('')
+streamlit.text("Thanks for adding " + add_my_fruit)
+
+#This will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
