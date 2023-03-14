fc1759320a6ff56406b1f0142ea99403be5b8790
JoleneSFTraining <127357900+JoleneSFTraining@users.noreply.github.com>
2023-03-14 16:33:25 -0500

    Removing a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,12 +43,12 @@ except URLError as e:
 
 # New Section to display fruityvice api response
 # import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
